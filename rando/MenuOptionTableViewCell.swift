7a9dc15522792e4bfb88a4b9acfd1051d119876f
Brando Flores <brandoflores16@gmail.com>
2021-07-09 19:53:10 -0700

    Add table view controller to side menu

---
+++ rando/MenuOptionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuOptionTableViewCell.swift
+//  rando
+//
+//  Created by Brando Flores on 7/9/21.
+//
+
+import UIKit
+
+class MenuOptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
