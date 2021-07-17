66a6d7d8194b910fcf30abad21c0bc5ee5d72998
Brando Flores <brandoflores16@gmail.com>
2021-07-16 19:21:28 -0700

    Stuff

--- rando/SideMenu/FeaturesViewController.swift
+++ rando/SideMenu/FeaturesViewController.swift
@@ -9,13 +9,9 @@ import UIKit
 
 class FeaturesViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -25,5 +21,3 @@ class FeaturesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
